bff3287af6b843957d6d13464077d0a74846bc77
Joshua Richardson-Noyes <admiraljrn@gmail.com>
2020-03-28 15:42:02 +0000

    Changes to RESTful api to support authentication Added in JSON Web Token check

--- index.js
+++ index.js
@@ -2,12 +2,16 @@
 const express = require("express");
 const knex = require("knex");
 const bodyParser = require("body-parser");
+const jwt = require("express-jwt");
 
 // Import back-end API code.
 const API = require("./api/api");
 
-// Load up config file for Database.
+// Load up config files
+// DB Config
 const dbConf = require("./conf/dbconn.json");
+// Security Configuration
+const conf = require("./conf/security.json");
 
 // Initialise SQL-Builder with Database Details.
 const db = knex({
@@ -22,6 +26,20 @@ app.set('db', db);
 
 // Parse POST body data.
 app.use(bodyParser());
+
+// Authorization - Don't let non-authenticated users work on extensions.
+app.use(jwt({
+    secret: conf.authSecret
+}))
+
+// App Handler in the event the user has not yet authenticated
+app.use((err, req, res, next) => {
+    if (err.name === 'UnauthorizedError') {
+        // TODO: Redirect to Lunasphere login page.
+        res.status(401);
+        res.send("<h2>Unauthorised</h2>");
+    }
+})
 
 // Setup API url.
 app.use('/api', API);
