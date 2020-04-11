920c02a12fd83f67c700058e53cbbb529a9407df
Kakutasuu <Kakutasu@hotmail.com>
2020-04-11 18:31:30 +0100

    Added newer version of XML builder in c#

---
+++ builder/builder/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Npgsql;
+using System.Xml;
+
+namespace builder
+{
+    class Program
+    {
+
+        static NpgsqlConnection Connection()
+        {
+            NpgsqlConnection conn = null;
+
+            string host, usr, pwd, db;
+            host = "localhost";
+            usr = "postgres";
+            pwd = "Passw0rd!";
+            db = "EXTENSIONS_DB";
+
+            string strConn = $@"Host={host};username={usr};password={pwd};database={db};";
+
+            try { conn = new NpgsqlConnection(strConn); }
+            catch(Exception e) { Console.WriteLine($"Couldn't connect to the database due to the following reasons: {e.Message}"); }
+
+            return conn;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing connection");
+
+            Connection().Open();
+
+            Connection().Close();
+        }
+    }
+}
